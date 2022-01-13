9cf3eca8cba5f25b7573592995749df3925c6c7d
Danya-Ps <95974350+Danya-Ps@users.noreply.github.com>
2022-01-13 14:41:06 +0300

    3.0

--- calc/getch_un.cpp
+++ calc/getch_un.cpp
@@ -3,10 +3,14 @@
 
 char buf[BUFSIZE]; /* буфер для ungetch */
 int bufp = 0; /* следующая свободная позиция в буфере */
+
+///Функция, берущая символ из буфера
 int getch(void) /* берем символ */
 {
 	return (bufp > 0) ? buf[--bufp] : getchar();
 }
+
+///Функция, возвращающая символ на вывод
 void ungetch(int c) /* возвращаем символ на ввод */
 {
 	if (bufp >= BUFSIZE)
